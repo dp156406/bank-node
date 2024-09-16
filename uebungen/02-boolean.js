32fb8d96183a8be3e0b584415a964f6befa9f30a
Philip <156904579+dp156406@users.noreply.github.com>
2024-09-16 07:08:06 +0000

    klobrille

--- uebungen/02-boolean.js
+++ uebungen/02-boolean.js
@@ -13,6 +13,20 @@ console.log("Der Ausdruck ist wahr.")
     console.log("Der Ausdruck ist unwahr / falsch / false")
 }
 
+// weil in dieser aufgabe die prüpfung immer wahr ist, wird immer die anweisung hinter if ausgeführt.
+// die anweisung hinter else kommt niemals zur ausführung.
+// Prüfoperatoren
+// == Prüfung auf gleichheit im wert
+//> größer als
+//< kleiner als
+//>= größer oder gleich
+//<= kleiner oder gleich
+//=== prüfung auf gleicheit im  wert und typ
+
+
+
+
+
 console.log("Aufgabe 02")
 
 // In Erweiterung von Aufgabe 1 soll der Ausdruck dynamisch entweder true oder false sein.
@@ -48,10 +62,61 @@ if (alter>17 && fuehrerschein === true){        //Wenn älter 17 und fuehrersche
 
 // Andere Lösung
 
-if(fuehrerschein){
+if(fuehrerschein)
 if(alter === 17){
     console.log("begleitet fahren")
 else{}
 }else{
     console.log("fahrradfahren"){}
 }
+ 
+
+let zahl = 1
+
+if(1){
+    console.log("DIe Zahl" + zahl + "nimmt den boolischen wert true an")
+}else{
+    console.log("Die Zahl" + zahl + "nimmt den boolischen wert false an")
+}
+
+// Positive Zahlen sind true. Die Ziffer null ist false.
+
+console.log("Aufgabe 5")
+
+// Auf einem Sparkonto soll bei einem positiven kontostand auf der Console stehen: "Zinsen berechnen"
+//Wenn kein Guthaben existiert: "keine Zinsen"
+
+let kontostand = 100
+
+if (kontostand > 0){
+    console.log("zinsen berechnen")
+}else{
+    console.log("keine Zinsen da kontostand)"
+    }
+
+console.log(Aufgabe 6)
+
+// wenn sieg dann als 3 punkte
+// wenn niederlage dann 0 punkte
+// wenn unentschieden dann 1 punkte
+
+let punkte = 10
+
+if (punkte > 0){
+    console.log("gewinner")
+}else{
+    console.log("verlierer")
+}
+
+if ("spielausgang == "sieg){
+    console.log("es gibt 3 punkte")
+}
+
+if ("spielausgang == "unentschieden){
+    console.log("es gibt 1 punkte")
+}
+
+if ("spielausgang == "niederlage){
+    console.log("es gibt 0 punkte")
+}
+require ()
