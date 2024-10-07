8ec293b9196d07f40302c05a39aae8760c5684df
Philip <156904579+dp156406@users.noreply.github.com>
2024-10-07 07:07:18 +0000

    ,kdmn vkushföiu

---
+++ Klausur Fn/test20243009.js
@@ -0,0 +1,101 @@
+console.log("Klausur 20240930")
+// 
+// Bearbeiten Sie alle Aufgaben nach Vorgabe. 
+// Deklarieren Sie stets Objekte mit Eigenschaften und sprechenden Namen und geben Sie einen Antwortsatz auf der Konsole aus.
+
+
+console.log("Aufgabe 1")
+// 1a) 
+// Es soll das Volumen eines Würfels in Quadratzentimeter (cm3) ausgegeben werden. Arbeiten Sie mit einem Objekt und sprechenden Eigenschaften.
+// Geben Sie das Volumen in einem Antwortsatz auf der Konsole aus.
+
+let a= 5;
+console.log('Volumen: ' + (a * a * a));
+//das ist der bauplan für würfel
+
+// Wenn das Volumen 1000 oder mehr Quadratzentimeter beträgt soll eine Meldung auf der Konsole ausgegeben werden, die besagt,
+// dass das Volumen 1000 oder mehr Quadratzentimeter beträgt.
+
+if (volumen) {
+  //  >1000         _77_
+    //77 deine      \`´/
+//                   /\
+das volumen beträgt mehr als 1000 Quadratzentimeter
+}
+this.v onvolumechange
+this.leck AuthenticatorResponse
+console.log("Aufgabe 2")
+// Sie werden beauftragt ein Schulverwaltungsprogramm für das BKB zu erstellen. Konkret sollen Sie sich in einem ersten Schritt
+// die Zjh 8qu zu meiner verteidigung du hundeugnisse versemen. Deklarieren, Instanziieren und Initialisieren Sie ein Zeugnisobjekt, 
+// in dem Sie neben "Deutsch", "Mathe" und "Englisch" 5 weitere, geeignete Eigenschaften deklarieren. 
+
+// 2a)loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+0plokololippokekhauansohn
+// Definieren Sie die class
+plokololippokekhauansohn
+class deutsch {
+    2 ()  {
+
+
+
+// 2b)
+// Deklarieren, Instanziieren und Initialisieren Sie ein Objekt von der Zeignisklasse.
+
+console.log("deutsch = 2")
+console.log("mathe = 3")
+console.log("englisch = 4"
+console.log("musik = 10")
+console.log("informatik =2")
+console.log("sport = 1")
+console.log("BWl = 2")
+console.log("Biologie = 4")
+// 2c)
+// Geben Sie die Eigenschaftswerte Ihres Objekts auf der Konsole aus.
+
+console.log("Aufgabe 3")
+// Es gilt:
+// Rabatt = Listenpreis · Rabatt-Prozentsatz : 100 %
+// Netto-Rechnungsbetrag = Listenpreis – Rabatt
+// MwSt = Netto-Rechnungsbetrag · MwSt-Prozentsatz : 100 %
+// MwSt = Netto-Rechnungsbetrag · MwSt-Prozentsatz : 100 %
+// Skonto = Brutto-Rechnungsbetrag · Skonto-Prozentsatz : 100 %
+// Zahlungsbetrag = Brutto-Rechnungsbetrag – Skonto
+
+// Der Netto-Rechnungsbetrag liegt bei 370,00 EUR.
+// Die Mehrwertsteuer hängt von der Produktart ab: 
+// * Lebensmittel, Zeitung und Buch: 7%
+// * alles andere: 19%
+// Dieses Produkt hat die Produktart "Zeitung"
+
+// 3a)
+// Deklarieren, Instanziieren Sie ein Objekt mit allen genannten Eigenschaften. 
+
+console.log("Rabatt = Listenpreis · Rabatt-Prozentsatz : 100 %")
+console.log("Netto-Rechnungsbetrag = Listenpreis – Rabatt")
+console.log("MwSt = Netto-Rechnungsbetrag · MwSt-Prozentsatz : 100 %")
+console.log("Skonto = Brutto-Rechnungsbetrag · Skonto-Prozentsatz : 100 %")
+console.log("Zahlungsbetrag = Brutto-Rechnungsbetrag – Skonto")
+
+// 3b
+// Initialisieren Sie Ihr Objekt mit den gegebenen Werten. 
+// Geben Sie die einen Anwortsatz auf der Konsole aus.
+
+if (zeitung) { then
+    7% MwSt
+}
+
+
+// 3c)
+// Berechnen Sie die MwSt in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
+
+if (MwSt7) { then (MwSt7)
+    
+} 
+
+
+
+// 3d)
+// Berechnen Sie den Eigenschaftswert des Brutto-Rechnungsbetrags. 
+// Wenn die Produktart sich ändert, muss Ihre Berechnung sich anpassen.
+// Geben Sie den Wert in einen Anwortsatz auf der Konsole aus.
